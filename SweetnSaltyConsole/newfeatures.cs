97709c771334359de648df35e5b7ab6651ad158e
briana wynn <briana.wynn@revature.net>
2021-09-14 09:41:53 -0700

    trying branching and merging again

---
+++ SweetnSaltyConsole/newfeatures.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SweetnSalty_Console
+{
+  class Newfeatures
+  {
+    Newfeatures()
+    {
+
+    }
+
+    public void AMethod()
+    {
+      //adding more new features because I messed up the branching the first time
+    }
+  }
+}
